a0bb12ae91b95ee55dd8aaf00a22beae71d08ec1
Masafumi Yokoyama <myokoym@gmail.com>
2013-08-19 23:26:44 +0900

    Add Osl::State::NumEffectState#generate

--- ext/osl/rbosl_state_numEffectState.hpp
+++ ext/osl/rbosl_state_numEffectState.hpp
@@ -1,6 +1,9 @@
 #include "ruby.h"
+#include "rbosl_move.h"
 #include <osl/state/numEffectState.h>
 #include <osl/handicap.h>
+#include <osl/container/moveVector.h>
+#include <osl/move_generator/legalMoves.h>
 #include <iostream>
 
 using namespace osl;
@@ -30,6 +33,23 @@ rb_numEffectState_show(VALUE self)
   return Qnil;
 }
 
+static VALUE
+rb_numEffectState_generate(VALUE self)
+{
+  state::NumEffectState* p;
+  Data_Get_Struct(self, state::NumEffectState, p);
+  MoveVector moves;
+  LegalMoves::generate(*p, moves);
+  VALUE moves_ary = rb_ary_new();
+  for (int i = 0; i < (int)moves.size(); i++) {
+    // TODO: ruby: munmap_chunk(): invalid pointer
+    //VALUE move = Data_Wrap_Struct(cMove, NULL, rb_move_free, &moves[i]);
+    VALUE move = Data_Wrap_Struct(cMove, NULL, NULL, &moves[i]);
+    rb_ary_push(moves_ary, move);
+  }
+  return moves_ary;
+}
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -40,6 +60,7 @@ Init_numEffectState(VALUE mState)
   cNumEffectState = rb_define_class_under(mState, "NumEffectState", rb_cObject);
   rb_define_singleton_method(cNumEffectState, "new", RUBY_METHOD_FUNC(rb_numEffectState_s_new), 0);
   rb_define_method(cNumEffectState, "show", RUBY_METHOD_FUNC(rb_numEffectState_show), 0);
+  rb_define_method(cNumEffectState, "generate", RUBY_METHOD_FUNC(rb_numEffectState_generate), 0);
 }
 #ifdef __cplusplus
 } /* extern "C" */
--- test/test-numEffectState.rb
+++ test/test-numEffectState.rb
@@ -4,4 +4,12 @@ class NumEffectStateTest < Test::Unit::TestCase
   def test_new
     assert_not_nil(Osl::State::NumEffectState.new)
   end
+
+  def test_generate
+    nstate = Osl::State::NumEffectState.new
+    moves = nstate.generate
+    assert_not_nil(moves)
+    assert_true(moves.is_a?(Array))
+    assert_true(moves.first.is_a?(Osl::Move))
+  end
 end
