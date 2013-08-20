ef020818750f20bdc3f210fa9c04f6f23e0bc004
Masafumi Yokoyama <myokoym@gmail.com>
2013-08-21 01:58:41 +0900

    Add first sample
    We can play Shogi by Ruby!!!

---
+++ sample/random_play.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+
+require "osl"
+
+state = Osl::State::NumEffectState.new
+
+loop do
+  # my turn
+  moves = state.generate
+  if moves.empty?
+    puts "make masita"
+    break
+  end
+  my_move = moves.sample
+  state.move(my_move)
+
+  state.show
+  puts my_move.to_csa
+
+  if state.in_check?
+    puts "checkmate!"
+    break
+  end
+
+  # your turn
+  op_move = nil
+  loop do
+    line = gets.chomp
+    if state.valid?(line)
+      op_move = line
+      break
+    end
+    puts "Invalid move. Please retry."
+  end
+
+  state.move(op_move)
+
+  state.show
+  puts op_move
+end
