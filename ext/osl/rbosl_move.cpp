efbf34dba19b221fccf960754f29817e8695db74
Masafumi Yokoyama <myokoym@gmail.com>
2013-08-21 00:04:39 +0900

    Add Osl::Move#to_csa

--- ext/osl/rbosl_move.cpp
+++ ext/osl/rbosl_move.cpp
@@ -1,6 +1,7 @@
 #include "ruby.h"
 #include "rbosl_move.h"
 #include <osl/move.h>
+#include <osl/record/csa.h>
 
 VALUE cMove;
 
@@ -20,6 +21,14 @@ rbosl_move_s_new(VALUE self)
   return Data_Wrap_Struct(self, NULL, rbosl_move_free, ptr);
 }
 
+static VALUE
+rbosl_move_to_csa(VALUE self)
+{
+  Move* ptr;
+  Data_Get_Struct(self, Move, ptr);
+  return rb_str_new2(record::csa::show(*ptr).c_str());
+}
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -28,6 +37,7 @@ rbosl_move_init(VALUE mOsl)
 {
   cMove = rb_define_class_under(mOsl, "Move", rb_cObject);
   rb_define_singleton_method(cMove, "new", RUBY_METHOD_FUNC(rbosl_move_s_new), 0);
+  rb_define_method(cMove, "to_csa", RUBY_METHOD_FUNC(rbosl_move_to_csa), 0);
 }
 #ifdef __cplusplus
 } /* extern "C" */
--- test/test-move.rb
+++ test/test-move.rb
@@ -4,4 +4,10 @@ class MoveTest < Test::Unit::TestCase
   def test_new
     assert_not_nil(Osl::Move.new)
   end
+
+  def test_to_csa
+    assert_true(Osl::Move.new.to_csa.is_a?(String))
+    nstate = Osl::State::NumEffectState.new
+    assert_match(/\+[0-9]{4}[A-Z]{2}/, nstate.generate.first.to_csa)
+  end
 end
