e850f2a6f16480dd37ac1c1b9ac4285bcd9fff7c
Masafumi Yokoyama <myokoym@gmail.com>
2013-08-20 20:36:36 +0900

    Add a missing include guard

--- ext/osl/rbosl_move.h
+++ ext/osl/rbosl_move.h
@@ -1,3 +1,6 @@
+#ifndef RBOSL_MOVE_H
+#define RBOSL_MOVE_H
+
 #include "ruby.h"
 #include <osl/move.h>
 
@@ -9,3 +12,5 @@ void rb_move_free(Move* ptr);
 static VALUE rb_move_s_new(VALUE self);
 
 void Init_move(void);
+
+#endif /* RBOSL_MOVE_H */
