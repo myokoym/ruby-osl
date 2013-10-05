ab2ccfc84642d0488bd10758527fe136197dd0e0
Masafumi Yokoyama <myokoym@gmail.com>
2013-10-06 01:30:58 +0900

    Build extension files before testing

--- test/run-test.rb
+++ test/run-test.rb
@@ -8,4 +8,13 @@ base_dir = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 $LOAD_PATH.unshift(File.join(base_dir, "lib"))
 $LOAD_PATH.unshift(File.join(base_dir, "test"))
 
+ext_dir = File.join(base_dir, "ext")
+ext_osl_dir = File.join(ext_dir, "osl")
+$LOAD_PATH.unshift(ext_osl_dir)
+require "fileutils"
+FileUtils.cd(ext_osl_dir) do
+  system("ruby", "extconf.rb")
+  system("make")
+end
+
 exit Test::Unit::AutoRunner.run(true)
